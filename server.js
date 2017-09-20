6153161c8049fc20cc30583c7f4c4c3f276ea24c
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-09-19 21:20:52 -0600

    upgrading to use my own simple server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use('/', express.static(__dirname + '/build'))
+app.use('/', express.static(__dirname + '/dist'))
 
-app.use(express.static(__dirname +'/static'));
+app.use(express.static(__dirname +'/static'))
 
-app.listen(80);
+app.listen(80)
