f9d55f49ca79f445b2305682960012150d057273
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-09-20 22:02:27 -0600

    https server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fs = require('fs')
+const https = require('https');
 const app = express()
 
 app.use('/', express.static(__dirname + '/dist'))
@@ -7,4 +9,15 @@ app.use('/img', express.static(__dirname + '/static/assets/'))
 
 app.use(express.static(__dirname +'/static'))
 
+try {
+  var httpsConfig = {
+    key: fs.readFileSync('/etc/letsencrypt/live/evetterdrake.me/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/evetterdrake.me/cert.pem'),
+  }
+
+  var httpsServer = https.createServer(httpsConfig, app)
+  httpsServer.listen(443)
+}
+catch(e){
+}
 app.listen(80)
